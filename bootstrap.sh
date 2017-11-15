545839611c53627fe2f268a4bdbb6be46be1f1d6
Sirio Balmelli <sirio.bm@gmail.com>
2017-11-15 20:34:05 +0100

    nix toolchain script

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,11 +19,13 @@ for cc in $compilers; do
 done
 
 # run valgrind
-pushd ./build-debugoptimized-gcc \
-	&& VALGRIND=1 meson test --wrap="valgrind --leak-check=full --show-leak-kinds=all"
-popd
+# TODO: won't fly on a 3.13.0-32-generic (ubuntu 12.04 EOL)
+#pushd ./build-debugoptimized-gcc \
+#	&& meson test --wrap="valgrind --leak-check=full --show-leak-kinds=all"
+#popd
 
 ## build and test sanitizers
+# TODO: wait for clang 4.0.1 bugfix in nix repos, or go with 5.0.0?
 #sanitizers="b_sanitize=address b_sanitize=thread" # Nix's clang-4.0.1 has no tsan?
 #for san in $sanitizers; do
 #	type="debugoptimized"
