eb59d3707fda59b1d3ba483eadc47ffef6270371
tony <tony@b-ad.ch>
2018-01-02 14:01:59 +0000

    build with nix from local dir

--- default.nix
+++ default.nix
@@ -19,12 +19,9 @@ stdenv.mkDerivation rec {
 		valgrind
 		python3
 	];
-	builder = ./builder.sh;
-	src = fetchgit {
-	  url = "https://github.com/TonyTheLion/nonlibc.git";
-	  rev = "e0ae6b9da45e355f5454174f630b097775c1c5a2";
-	  sha256 = "1pwkiq2vs0lzkd4xww2wj39q7r0cj88gvdghy3hzh4246n9za7fl";
-	  fetchSubmodules = true;
-	}; 
+	src = ./.;
+	buildPhase = "
+		ninja
+		ninja install";
 	inherit meson;
 }
