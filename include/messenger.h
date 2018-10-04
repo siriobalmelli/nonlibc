995befbc1d733eb6e4aab6374a3312ba5841ac35
Sirio Balmelli <sirio@b-ad.ch>
2018-10-04 15:45:03 +0200

    initial messenger draft

---
+++ include/messenger.h
@@ -0,0 +1,54 @@
+#ifndef messenger_h_
+#define messenger_h_
+
+/* messenger - the atomic pipe-based messaging library
+ *
+ * Leverage the fact that PIPE_BUF is guaranteed atomic to pass simple messages
+ * between threads which are listening (e.g. with epoll) to a pipe.
+ *
+ * Useful when avoiding global state by passing messages between threads/coroutines,
+ * or when the program has run-to-sleep semantics (wakelocks on Android).
+ *
+ * Provides a simple "group" registration and message broadcast mechanism
+ * using RCU to track membership.
+ *
+ * Eminently thread-safe.
+ *
+ * (c) 2018 Sirio Balmelli and Anthony Soenen
+ */
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <limits.h> /* PIPE_BUF */
+#include <stdlib.h>
+
+/*
+ *	message passing functions
+ * NOTE: these are orthogonal to mg_subscribe/unsubscribe/alert (messenger)
+ * semantics below/
+ */
+
+/* pipe() only guarantees atomicity for PIPE_BUF bytes */
+#define MG_MAX (PIPE_BUF - sizeof(struct message))
+
+ssize_t mg_send(int to_fd, void *data, size_t len);
+ssize_t mg_recv(int from_fd, void *data_out);
+
+
+/*
+ *	message passing groups
+ */
+struct mg_group {
+	/* TODO: proper type for RCU */
+	void	*fd_rcu;
+};
+
+void mgrp_free(struct mg_group *grp);
+struct mg_group *grprp_new();
+
+int mgrp_subscribe(struct mg_group *grp, int my_fd);
+int mgrp_unsubscribe(struct mg_group *grp, int my_fd);
+int mgrp_broadcast(struct mg_group *grp, void *data, size_t len);
+
+#endif /* messenger_h_ */
---
+++ lib/messenger.c
@@ -0,0 +1,127 @@
+#include <zed_dbg.h>
+#include <nonlibc.h>
+#include <messenger.h>
+
+/* our 'size' variable on this platform *must* be large enough
+ * to express the largest atomic write size for a pipe (PIPE_BUF)
+ */
+NLC_ASSERT(mg_size_sanity, INT_FAST16_MAX > PIPE_BUF);
+
+struct message {
+union{
+struct {
+	uint_fast16_t	len;
+	uint8_t		data[];
+};
+	uint8_t		bytes[PIPE_BUF];
+};
+};
+
+
+/* macrofy it so that we don't rewrite this for mgrp_broadcast()
+ * or (heaven forbid) do a new "assembly" for each target of the broadcast.
+ */
+#define MG_ASSEMBLE  \
+	if (len > MG_MAX) \
+		return -1; \
+	struct message mg = { .len = len }; \
+	memcpy(mg.data, data, len);
+
+
+/* mg_send()
+ * Write 'len' bytes from 'data' into 'to_fd'.
+ * Return behavior and semantics are same as for write()
+ */
+ssize_t mg_send(int to_fd, void *data, size_t len)
+{
+	MG_ASSEMBLE
+	ssize_t ret = write(to_fd, &mg, PIPE_BUF);
+	/* hide our header size from the caller */
+	if (ret > 0)
+		return len;
+	return ret; /* because there is a different between 0 and -1 */
+}
+
+/* mg_recv()
+ * Get a message in 'from_fd' and write it to 'data_out'.
+ * Return behavior and semantics are same as for read().
+ */
+ssize_t mg_recv(int from_fd, void *data_out)
+{
+	/* NOTE: we assume that since we are writing *atomically*
+	 * we will *always* read either PIPE_BUF or 0 or -1
+	 */
+	struct message mg;
+	ssize_t rd = read(from_fd, &mg, PIPE_BUF);
+	if (rd != PIPE_BUF)
+		return rd;
+
+	ssize_t ret = (ssize_t)mg.len;
+	memcpy(data_out, mg.data, ret);
+	return ret;
+}
+
+
+/* mgrp_free()
+ */
+void mgrp_free(struct mg_group *grp)
+{
+	if (!grp)
+		return;
+	/* TODO: free RCU */
+	free(grp);
+}
+
+/* mgrp_new()
+ * Create a new messaging group, return pointer,
+ * which must be freed with mgrp_free().
+ */
+struct mg_group *mgrp_new()
+{
+	struct mg_group *grp = NULL;
+	Z_die_if(!(
+		grp = malloc(sizeof *grp)
+		), "");
+	/* TODO: init rcu linked list */
+	return grp;
+out:
+	mgrp_free(grp);
+	return NULL;
+}
+
+/* mgrp_subscribe()
+ * Subscribe 'my_fd' to receive notifications by 'grp'.
+ * Returns 0 on success.
+ */
+int mgrp_subscribe(struct mg_group *grp, int my_fd)
+{
+	/* TODO: add 'my_fd' to RCU linked list */
+}
+
+/* mgrp_unsubscribe()
+ * Remove 'my_fd' from 'grp'.
+ * Returns 0 on success.
+ */
+int mgrp_unsubscribe(struct mg_group *grp, int my_fd)
+{
+	/* TODO: remove from RCU linked list */
+}
+
+/* mgrp_broadcast()
+ * Send 'len' bytes from 'data' to each member of 'grp'.
+ * Same semantics as write().
+ */
+int mgrp_broadcast(struct mg_group *grp, void *data, size_t len)
+{
+	int err_cnt = 0;
+	int curr_fd = 0;
+	MG_ASSEMBLE
+	/* TODO: walk linked list
+	while (curr_fd = rcu_next(grp->fd_rcu))
+	*/
+		Z_err_if(write(curr_fd, &mg, PIPE_BUF) != PIPE_BUF,
+			"curr_fd %d", curr_fd);
+	if (err_cnt)
+		return -1;
+	return len;
+}
