c2c2869acd2a34c61157710dc1a48eed3cbe917a
Sirio Balmelli <sirio@b-ad.ch>
2022-09-11 14:58:45 +0200

    cleanup build

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
-gem "nokogiri", "~> 1.10.10"
 gem 'jekyll'
 
 group :jekyll_plugins do
--- jekyll.nix
+++ jekyll.nix
@@ -2,25 +2,44 @@
   nixpkgs ? import (builtins.fetchGit {
     url = "https://siriobalmelli@github.com/siriobalmelli-foss/nixpkgs.git";
     ref = "refs/tags/sirio-2022-08-24";
-    }) {}
+    }) { },
   }:
 
 with nixpkgs;
 
-let jekyll_env = bundlerEnv rec {
-    name = "jekyll_env";
-    gemfile = ./Gemfile;
-    lockfile = ./Gemfile.lock;
-    gemset = ./gemset.nix;
-  };
+let
+  rubyEnv = ruby.withPackages (p: with p; [
+    github-pages
+    jekyll
+    jekyll-theme-minimal
+  ]);
+
 in
   stdenv.mkDerivation rec {
-    name = "jekyll_env";
-    buildInputs = [ jekyll_env ];
+    name = "nonlibcDocumentation";
 
+    buildInputs = [
+      rubyEnv
+    ];
+
+    # work with the current directory. ignore all files not in version control
     src = if lib.inNixShell then null else nix-gitignore.gitignoreSource [] ./.;
 
+    buildPhase = ''
+        bundle exec jekyll build -d "$out"
+    '';
+    dontInstall = true;
+
+    # scripts and executables served by Jekyll must run on systems that download them
+    # which would be broken if their #! were patched to /nix/store/[derivation]
+    dontStrip = true;
+    dontMoveSbin = true;
+    dontPatchELF = true;
+    dontPatchShebangs = true;
+
     shellHook = ''
-      exec ${jekyll_env}/bin/jekyll serve --watch
+        rm -rf .bundle
+        echo "# to build and serve locally, run:"
+        echo "jekyll serve --watch"
     '';
 }
